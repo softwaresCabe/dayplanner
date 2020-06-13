560e9573f66564766ba04f56110ebc9a0ba944d8
nivek26 <nivek2696@gmail.com>
2020-06-12 22:44:03 -0700

    working example

--- script.js
+++ script.js
@@ -1,114 +1,122 @@
-function createHour(index){
+$(document).ready(function(){
 
-  var hour;
-  var isPast;
-  var iscurrent;
-  var isfuture;
+  getDate();
 
-  function assignHour(){
+  createAllHours();
 
-    switch(index) {
-      case 0: 
-        hour = "9:00am";
-        break;
-      case 1:
-        hour = "10:00am";
-        break;
+  $(".savebtn").on("click", saveBtnClicked );
 
-      case 2: 
-        hour = "11:00am";
-        break;
-
-      case 3:
-        hour = "12:00pm";
-        break;
-
-      case 4: 
-        hour = "1:00pm";
-        break;
-
-      case 5:
-        hour = "2:00pm";
-        break;
-
-      case 6: 
-        hour = "3:00pm";
-        break;
-
-      case 7:
-        hour = "4:00pm";
-        break;
-
-      case 8:
-        hour = "5:00pm";
-      
-    }
-  }
-
-
-
-
-
-  var beginningTime = moment("12:00pm", 'h:mma');
-  var endTime = moment(hour, 'h:mma');
-  console.log(beginningTime.isBefore(endTime));
-
-  assignHour();
-
-  $(".container").append('<div class="d-flex flex-row hourarea"><div class ="col-1 time" class="areatime"> '+ hour +' </div><textarea class="form-control col-9 justify-content-start" style="background-color: green;"></textarea><button type="button" class="savebtn btn btn-primary col-2 justify-content-end"> Save </button</div>');
-
-
-}
-
-
-function getDate(){
-
-    $("#todaysinfo").text( moment().format('LL') );
-
-}
+})
 
 
 function saveBtnClicked(){
 
-  var btnObj = $(this);
+  var scheduleText = $(this).siblings(".form-control").val();
+  var scheduleTime = $(this).attr("hour");
 
-  var dataToSave = $( "#hourarea textarea").val();
-  //var timeTosave =  $( "#hourarea .time" ).;
-
-  console.log(timeTosave);
-
-
-  //localStorage.setItem("hour", );
-
-  localStorage.setItem("task", dataToSave);
-
+  //localStorage.setItem("hour", scheduleTime);
+  localStorage.setItem(scheduleTime, scheduleText);
 
 }
 
 
 function createAllHours(){
-
   for(var i = 0; i < 9; i ++){
     createHour(i);
-
   }
 }
 
-$(document).ready(function(){
 
-  getDate();
+function createHour(index){
+  var hour;
+
+  var container = $(".container");
+
+  //assign each hour of each hour block
+  hour = assignHour(index, hour);
+
+  // assign a color to each div based on hour os the day
+  var currentHour = moment().format("HH");
+  var color = "";
+
+  if((index + 9) === currentHour ){
+    color = "red";
+
+  }else if ((index + 9) < currentHour ){
+    color = "lightgray";
+  }else{
+    color ="green";
+  }
+
+  //Get Local storage Data and assign text area
+  var storedData = "";
+
+  if( localStorage.getItem(index + 9) != undefined ){
+    storedData = localStorage.getItem(index + 9);
+  }
 
 
-  //$("#hourarea .savebtn").on("click", saveBtnClicked );
+  var divHTML = '<div hour="'+ (index + 9) +'" class="d-flex flex-row hourarea">' +
+  '<div class ="col-1 time">' + hour + ' </div>'+
+  '<textarea class="form-control col-9 justify-content-start" style="background-color: '+ color +';">' + storedData + '</textarea>'+
+  '<button hour="'+ (index + 9) +'" type="button" class="savebtn btn btn-primary col-2 justify-content-end"> Save </button>'+
+  '</div>';
 
-  createAllHours();
+  container.append(divHTML);
+
+  //Get Local storage Data and assign text area
+  var storedData;
+
+  if( localStorage.getItem(index + 9) != undefined ){
+    storedData = localStorage.getItem(index + 9);
+  }
+}
 
 
-})
+function getDate(){
+    $("#todaysinfo").text( moment().format('LL') );
+}
 
 
-console.log( moment().startOf('day').fromNow() );
+function assignHour(index){
+  switch(index) {
+    case 0: 
+      hour = "9:00am";
+      break;
+    case 1:
+      hour = "10:00am";
+      break;
 
+    case 2: 
+      hour = "11:00am";
+      break;
+
+    case 3:
+      hour = "12:00pm";
+      break;
+
+    case 4: 
+      hour = "1:00pm";
+      break;
+
+    case 5:
+      hour = "2:00pm";
+      break;
+
+    case 6: 
+      hour = "3:00pm";
+      break;
+
+    case 7:
+      hour = "4:00pm";
+      break;
+
+    case 8:
+      hour = "5:00pm";
+  }
+
+  return hour;
+}
 
 
 
